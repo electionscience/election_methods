9420d41757e9b5e2e8d09d8ac5ffd413768397c3
Felix Sargent <felix@truework.com>
2022-04-19 12:50:19 -0400

    Replaced my STV with Andy's STV because I believe it's better

--- single_transferrable_vote.py
+++ single_transferrable_vote.py
@@ -1,8 +1,7 @@
-# Single Transferrable Vote
-import pandas
-import numpy
+import numpy as np
+import pandas as pd
 
-# Ranking a scored ballot because ranked ballots are bad, and annoying to compose.
+
 ballots = [
 
     {"A": 5, "B": 4, "C": 3, "D": 0, "E": 0, "F": 0},
@@ -112,59 +111,40 @@ ballots = [
 ]
 
 seats = 3
-ballots = pandas.DataFrame(ballots)
-quota = 1+numpy.floor(len(ballots) / (seats + 1))
-seated = []
-
-weights = pandas.Series(numpy.ones(len(ballots)), name='weight')
+ballots = pd.DataFrame(ballots)
+quota = 1+np.floor(len(ballots) / (seats + 1))
 
 
-def choices(ballots):
-    return pandas.DataFrame(ballots.apply(lambda x: x.idxmax(), axis=1), columns=['choice'])
+def STV(ballots, seats, quota):
+    V,C = ballots.shape
 
+    weights = np.ones(V)
+    elected = []
+    remaining = list(ballots.columns)
 
-def summarize(ballots):
-    # count highest rated candidate from each row, then count the number of times it occurs
-    return pandas.concat([choices(ballots), weights], axis=1).groupby('choice').sum()
+    for _ in range(seats):
+        y = (ballots.max(axis=1))
+        y = pd.concat([y]*len(remaining), axis=1).rename(columns = lambda i: ballots.columns[i])
+        fp = (y == ballots)
+        support = fp.mul(weights, axis=0).sum(axis=0)
 
+        while support.max() < quota and len(remaining) > seats - len(elected):
+            remaining.remove(support.idxmin())
+            ballots = ballots[remaining]
 
-def eliminate_zeros(ballots):
-    # eliminate candidates who have no first place votes
-    eliminated = ballots.columns.difference(summarize(ballots).index)
-    if not eliminated.empty: 
-        print(f"{eliminated[0] = }, no first place votes")
-    return ballots.drop(eliminated, axis=1)
+            y = (ballots.max(axis=1))
+            y = pd.concat([y]*len(remaining), axis=1).rename(columns = lambda i: ballots.columns[i])
+            fp = (y == ballots)
+            support = fp.mul(weights, axis=0).sum(axis=0)
 
+        winner = support.idxmax()
+        surplus = max(1, support.max()/quota)
+        weights[fp[winner]] *= (1 - 1/surplus)
 
-def eliminate_lowest(ballots):
-    # if no candidate meets the quota, eliminate the worst performing candidate
-    round_count = summarize(ballots)
-    if round_count[round_count['weight'] >= quota].empty:
-        eliminated = round_count.idxmin().values[0]
-        print(f"{eliminated[0] = }, no candidate meets quota, eliminated lowest")
-        return ballots.drop(eliminated, axis=1)
-    return ballots
+        elected.append(winner)
+        remaining.remove(winner)
+        ballots = ballots[remaining]
 
+    return elected
 
-def elect_and_distribute_excess(ballots, weights):
-    round_count = summarize(ballots)
-    # Elect the candidates who have passed the quota
-    winner = round_count[round_count['weight'] >= quota].index[0]
-    seated.extend(winner)
-    excess_votes = round_count.loc[winner].values[0] - quota
-    print(f"{seated =}, {quota = }, {excess_votes = }")
-    fractional_votes = excess_votes / round_count.max()
-    # divide the excess votes by the number of votes for the winner for the value of the excess votes
-
-    # find the second choice votes of those who voted for the winner
-    voted_for_winner = choices(ballots)[choices(ballots) == winner].dropna()
-    ballots = ballots.drop(winner, axis=1)
-    # redistribute the excess votes to the second choices
-    weights.iloc[voted_for_winner.index] = fractional_votes
-    return ballots, weights
-
-
-while len(seated) < seats:
-    ballots = eliminate_zeros(ballots)
-    ballots = eliminate_lowest(ballots)
-    ballots, weights = elect_and_distribute_excess(ballots, weights)
+print (STV(ballots, seats, quota))
