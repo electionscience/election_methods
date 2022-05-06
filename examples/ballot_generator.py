6ba7e561797b2da317f2ead40a8ce199754f6a5e
Felix Sargent <fsargent@users.noreply.github.com>
2022-05-06 10:48:20 -0700

    Move methods from ipynb and .py to methods.py and use imports. (#22)
    * Move methods from ipynb and .py to methods.py and use imports.
    
    * Move SPAV to use ballot generator
    
    * Fix import errors
    
    * Don't cache dependencies
    
    * Fix deploy formatting
    
    * Reformat Ballot Generator

--- examples/ballot_generator.py
+++ examples/ballot_generator.py
@@ -1,45 +1,140 @@
-import numpy as np
+import itertools
 import pandas as pd
 
 
-def STV(ballots, seats, quota):
-    V, C = ballots.shape
+def generate_approval_ballots():
+    # Compose Ballots
+    # candidates = ["Squash", "Potato", "Broccoli", "Blueberry", "Strawberry", "Banana"]
+    candidates = ["A", "B", "C", "X", "Y", "Z"]
+    # candidates = ["🥕", "🥔", "🥦", "🫐", "🍓", "🍌"]
 
-    weights = np.ones(V)
-    elected = []
-    remaining = list(ballots.columns)
+    ballot1 = {
+        candidates[0]: 1,
+        candidates[1]: 1,
+        candidates[2]: 1,
+        candidates[3]: 0,
+        candidates[4]: 0,
+        candidates[5]: 0,
+    }
+    ballot2 = {
+        candidates[0]: 0,
+        candidates[1]: 1,
+        candidates[2]: 1,
+        candidates[3]: 0,
+        candidates[4]: 0,
+        candidates[5]: 0,
+    }
+    ballot3 = {
+        candidates[0]: 1,
+        candidates[1]: 1,
+        candidates[2]: 1,
+        candidates[3]: 1,
+        candidates[4]: 0,
+        candidates[5]: 0,
+    }
+    ballot4 = {
+        candidates[0]: 0,
+        candidates[1]: 0,
+        candidates[2]: 0,
+        candidates[3]: 1,
+        candidates[4]: 1,
+        candidates[5]: 1,
+    }
+    ballot5 = {
+        candidates[0]: 0,
+        candidates[1]: 0,
+        candidates[2]: 1,
+        candidates[3]: 1,
+        candidates[4]: 1,
+        candidates[5]: 1,
+    }
+    ballot6 = {
+        candidates[0]: 0,
+        candidates[1]: 0,
+        candidates[2]: 0,
+        candidates[3]: 1,
+        candidates[4]: 1,
+        candidates[5]: 0,
+    }
 
-    for _ in range(seats):
-        y = ballots.max(axis=1)
-        y = pd.concat([y] * len(remaining), axis=1).rename(
-            columns=lambda i: ballots.columns[i]
-        )
-        fp = y == ballots
-        support = fp.mul(weights, axis=0).sum(axis=0)
-
-        while support.max() < quota and len(remaining) > seats - len(elected):
-            remaining.remove(support.idxmin())
-            ballots = ballots[remaining]
-
-            y = ballots.max(axis=1)
-            y = pd.concat([y] * len(remaining), axis=1).rename(
-                columns=lambda i: ballots.columns[i]
-            )
-            fp = y == ballots
-            support = fp.mul(weights, axis=0).sum(axis=0)
-
-        winner = support.idxmax()
-        surplus = max(1, support.max() / quota)
-        weights[fp[winner]] *= 1 - 1 / surplus
-
-        elected.append(winner)
-        remaining.remove(winner)
-        ballots = ballots[remaining]
-
-    return elected
+    ballots = (
+        list(itertools.repeat(ballot1, 112))
+        + list(itertools.repeat(ballot2, 6))
+        + list(itertools.repeat(ballot3, 4))
+        + list(itertools.repeat(ballot4, 73))
+        + list(itertools.repeat(ballot5, 4))
+        + list(itertools.repeat(ballot6, 1))
+    )
+    ballots = pd.DataFrame(ballots)
+    return ballots
 
 
-if __name__ == "__main__":
+def generate_score_ballots():
+    # Compose Ballots
+    candidates = ["Squash", "Potato", "Broccoli", "Blueberry", "Strawberry", "Banana"]
+    # candidates = ["🥕", "🥔", "🥦", "🫐", "🍓", "🍌"]
+
+    ballot1 = {
+        candidates[0]: 5,
+        candidates[1]: 5,
+        candidates[2]: 5,
+        candidates[3]: 0,
+        candidates[4]: 0,
+        candidates[5]: 0,
+    }
+    ballot2 = {
+        candidates[0]: 3,
+        candidates[1]: 5,
+        candidates[2]: 5,
+        candidates[3]: 2,
+        candidates[4]: 2,
+        candidates[5]: 2,
+    }
+    ballot3 = {
+        candidates[0]: 5,
+        candidates[1]: 5,
+        candidates[2]: 5,
+        candidates[3]: 5,
+        candidates[4]: 0,
+        candidates[5]: 0,
+    }
+    ballot4 = {
+        candidates[0]: 0,
+        candidates[1]: 0,
+        candidates[2]: 0,
+        candidates[3]: 5,
+        candidates[4]: 5,
+        candidates[5]: 4,
+    }
+    ballot5 = {
+        candidates[0]: 0,
+        candidates[1]: 0,
+        candidates[2]: 4,
+        candidates[3]: 5,
+        candidates[4]: 5,
+        candidates[5]: 5,
+    }
+    ballot6 = {
+        candidates[0]: 0,
+        candidates[1]: 0,
+        candidates[2]: 0,
+        candidates[3]: 5,
+        candidates[4]: 5,
+        candidates[5]: 3,
+    }
+
+    ballots = (
+        list(itertools.repeat(ballot1, 112))
+        + list(itertools.repeat(ballot2, 6))
+        + list(itertools.repeat(ballot3, 4))
+        + list(itertools.repeat(ballot4, 73))
+        + list(itertools.repeat(ballot5, 4))
+        + list(itertools.repeat(ballot6, 1))
+    )
+    return pd.DataFrame(ballots)
+
+
+def generate_ranked_ballots():
     ballots = [
         {"A": 5, "B": 4, "C": 3, "D": 0, "E": 0, "F": 0},
         {"A": 5, "B": 4, "C": 3, "D": 0, "E": 0, "F": 0},
@@ -141,9 +236,4 @@ if __name__ == "__main__":
         {"A": 0, "B": 3, "C": 5, "D": 4, "E": 0, "F": 0},
         {"A": 0, "B": 3, "C": 5, "D": 4, "E": 0, "F": 0},
     ]
-
-    seats = 3
-    ballots = pd.DataFrame(ballots)
-    quota = 1 + np.floor(len(ballots) / (seats + 1))
-
-    print(STV(ballots, seats, quota))
+    return pd.DataFrame(ballots)
